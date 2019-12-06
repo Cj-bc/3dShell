54b6aa653746ac62f8f33491228df544f4ab1600
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-06 18:49:13 +0900

    Add: Assets/Scripts> Directory/File/Type

---
+++ Assets/Scripts/Type.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Type {
+    enum FileType { Normal
+                  , Image
+                  , Movie
+                  }
+}
