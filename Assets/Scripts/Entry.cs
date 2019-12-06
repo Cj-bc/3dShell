9e7efa136bfb04b3ae9303463ebd36fd7ffc1b44
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-06 18:47:37 +0900

    Add: Assets/Scripts/Entry>

---
+++ Assets/Scripts/Entry.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Type
+
+public class Entry : MonoBehaviour
+{
+    public string name;
+    public Entry  parent;
+    public byte permission;
+    public Prefab model;
+}
