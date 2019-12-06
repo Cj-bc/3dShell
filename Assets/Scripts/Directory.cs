937d2c7ca85e01a4d63a207f8de8f02cb06b0a1a
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-06 22:01:55 +0900

    Change: Scripts/Directory> inherits Entry

--- Assets/Scripts/Directory.cs
+++ Assets/Scripts/Directory.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Directory : MonoBehaviour
+public class Directory : Entry
 {
     public List<Entry> children;
     // Start is called before the first frame update
