bf3fb90662406060084d82136e790a75815043cc
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-07 17:52:59 +0900

    Add: Scripts/File> add constructor with one arg 'path'

--- Assets/Scripts/File.cs
+++ Assets/Scripts/File.cs
@@ -6,4 +6,11 @@ public class File : Entry
 {
     public Type.FileType type;
 
+    public File(string _path) : base (_path) {
+        type = ftdetect();
+    }
+
+    private Type.FileType ftdetect() {
+      return Type.FileType.Normal;
+    }
 }
