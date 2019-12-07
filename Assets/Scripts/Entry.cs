0d0c9f8e944aaba223d87f7601cda4e74bfe8784
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-08 00:04:04 +0900

    Fix: Scripts/Entry> avoid name conflict

--- Assets/Scripts/Entry.cs
+++ Assets/Scripts/Entry.cs
@@ -8,7 +8,7 @@ using Type;
 public class Entry : MonoBehaviour
 {
     public string path;
-    public string name;
+    public string entryName;
     public Entry  parent;
     public int permission;
     public System.DateTime lastTouched;
@@ -16,7 +16,7 @@ public class Entry : MonoBehaviour
 
     public Entry(string _path) {
         path = _path;
-        name = _path.Split('/').Last();
+        entryName = _path.Split('/').Last();
         parent = null;
         permission = 0b111000000; // TODO: get this from data
         lastTouched = System.DateTime.FromFileTime(0); // TODO: get this from data
