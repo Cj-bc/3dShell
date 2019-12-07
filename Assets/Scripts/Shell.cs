4b2630bdcbc78503ee2ee85a301dd226c9d5417f
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-08 00:06:36 +0900

    Add: Scripts/Shell> hold state and manipulate whole shell

---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dir = System.IO.Directory;
+using Type;
+
+public class Shell : MonoBehaviour
+{
+    Directory pwd;
+    List<GameObject> entries = new List<GameObject>();
+    // User currentUser;
+    RuntimePlatform os;
+
+    Config config;
+
+    void Awake()
+    {
+        config = FindObjectOfType<Config>();
+        // currentUser = System.Environment.UserName;
+        pwd = new Directory(Dir.GetCurrentDirectory(), config);
+        updateEntryObjects();
+    }
+
+    void updateEntryObjects() {
+        if (entries.Count != 0)
+            foreach (GameObject obj in entries) {Destroy(obj);}
+
+        foreach (Entry e in pwd.children) {
+            GameObject newObj = Instantiate(e.model);
+        }
+    }
+}
