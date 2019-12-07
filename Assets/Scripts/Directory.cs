594e21ea855e0ec7fc90e43dd4c18d3da9ab9780
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-07 15:33:35 +0900

    Add: Scripts/Directory> getActualChildren/getDirectoryStub

--- Assets/Scripts/Directory.cs
+++ Assets/Scripts/Directory.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Dir = System.IO.Directory;
 
 public class Directory : Entry
 {
     private List<Entry> _children = new List<Entry>();
+
+
+    private List<Entry> getActualChildren() {
+        IEnumerable<string> files       = Dir.EnumerateFiles(path);
+        IEnumerable<string> directories = Dir.EnumerateDirectories(path);
+        List<Entry> ret = new List<Entry>();
+
+        foreach (string f in files) { ret.Add(new File(f)); }
+        foreach (string d in directories) { ret.Add(getDirectoryStub(d)); }
+
+        return ret;
+    }
+
+    // Create stub directory instance that doesn't hold children
+    // This is required so that parent Directory holds only child dirs/files,
+    // not grandchildren /great-grandchildren, ...
+    public static Directory getDirectoryStub(string d) => (Directory) new Entry(d);
+
 }
