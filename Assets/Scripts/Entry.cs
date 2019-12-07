f2d684c9c7c9f76e11210fb4063efe14f88cf802
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-07 15:15:55 +0900

    Fix: Scripts/Entry> some compile error

--- Assets/Scripts/Entry.cs
+++ Assets/Scripts/Entry.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using Type
+using Type;
 
 public class Entry : MonoBehaviour
 {
+    public string path;
     public string name;
     public Entry  parent;
     public byte permission;
-    public DateTime lastTouched;
-    public Prefab model;
+    public System.DateTime lastTouched;
+    public GameObject model;
 }
